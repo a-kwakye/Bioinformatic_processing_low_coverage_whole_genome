17ae36f559012757836cdba87de070038fd90386
Alexander Kwakye <51373937+a-kwakye@users.noreply.github.com>
2025-09-06 06:01:21 -0400

    Update sample_list.py

--- sample_list.py
+++ sample_list.py
@@ -1,6 +1,6 @@
 import os
 
-for root, dirs, files in os.walk(os.path.abspath("/gpfs/scratch/akwakye/Warfle_22_23_24/F24A430001458_LIBucnlR/")):
+for root, dirs, files in os.walk(os.path.abspath("/path/to/your/reads/")):
     for file in files:
         if file.endswith(".gz"):
             print(os.path.join(root, file))
